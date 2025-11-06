d2d89ce594b2c325af49dbdcf711531a542559fa
Navi-Nero <isaac.f.quinto@gmail.com>
2025-11-07 00:57:07 +0800

    Applied fixes to handle scanner exception.

--- Games/Input_Handling.java
+++ Games/Input_Handling.java
@@ -4,39 +4,42 @@ import java.util.Scanner;
 
 public class Input_Handling {
 
-    public int getInt (String dialouge) {
-        Scanner sc =  new Scanner(System.in);
+    private final Scanner sc;
+
+    //Open Scanner
+    public Input_Handling() {
+        sc = new Scanner(System.in);
+    }
+
+    public int getInt(String dialogue) {
         int getInt;
 
         while (true) {
-            System.out.print(dialouge);
+            System.out.print(dialogue);
 
             if (sc.hasNextInt()) {
                 getInt = sc.nextInt();
                 sc.nextLine();
                 break;
-
             } else {
                 System.out.println("Invalid input! Please enter an integer.");
                 sc.nextLine();
             }
         }
 
-        sc.close();
         return getInt;
     }
 
-    public String getStr (String dialouge) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print(dialouge);
-        sc.close();
+    public String getStr(String dialogue) {
+        System.out.print(dialogue);
         return sc.nextLine();
     }
 
-    public int randomize (int n) {
-        int i = (int)(Math.random() * n);
+    public int randomize(int n) {
+        return (int)(Math.random() * n);
+    }
 
-        return i;
-    }  
+    public void close() {
+        sc.close();
+    }
 }
