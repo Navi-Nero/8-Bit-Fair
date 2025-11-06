8f6034016b0438677251cc81610ef1dca2774d84
Navi-Nero <isaac.f.quinto@gmail.com>
2025-11-06 18:13:30 +0800

    Add files via upload

---
+++ Wordle.java
@@ -0,0 +1,26 @@
+package Games;
+
+import Games.Wordle_Assets.Process_Wordle;
+
+public class Wordle extends Process_Wordle {
+
+    public static void main(String[] args) {
+
+        Input_Handling input = new Input_Handling();
+
+        System.out.println("\nWelcome to wordle! How would you like to start the game?");
+
+        boolean restart = true;
+        while (restart == true) {
+
+            System.out.println("1. Have another player enter the word to guess.");
+            System.out.println("2. Generate a random word.");
+            
+            String guessWord = Proccess.chooseMode();
+            
+            Proccess.prepareGame(guessWord);
+
+            restart = Verify.restartGame();
+        }
+    }
+}
